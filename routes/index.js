48ab00d879b6d0b0c35b2fa8b41b3993eeee8ac6
sumaolin <sumaolin619@yahoo.cn>
2013-12-04 16:17:34 +0800

    测试代码

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 /*
  * GET home page.
  */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function(app){
+	app.get('/nswbmw',function(req, res){
+		res.send("Hello World");
+	})
+	// app.get('/',function(req, res){
+	// 	res.render('index',{title:"Express 3"});
+	// });
+}
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
