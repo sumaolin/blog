5274a567d04138d2e57606c2a3601a461a51cbf7
sumaolin <sumaolin619@yahoo.cn>
2013-12-06 20:11:53 +0800

    用户注册代码的实现

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@
 /*
  * GET home page.
  */
+
+
+var crypto = require('crypto'), //用它生成散列值来加密密码
+	user = require('../models/user');
+
 module.exports = function(app){
 	app.get('/',function(req, res){
 
@@ -13,7 +18,38 @@ module.exports = function(app){
 	});
 
 	app.post('/reg', function(req, res){
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password_re'];
+		if(password_re != password){
+			req.flash('error','两次输入的密码不一致！');
+			return res.redirect('/reg');
+		}
 
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+
+		User.get(newUser.name, function(err,user){
+			if(user){
+				req.flash('error','用户已存在！');
+				return res.redirect('/reg');
+			}
+
+			newUser.save(function(err, user){
+				if(err){
+					req.flash('error',err);
+					res.redirect('/reg');
+				}
+				req.session.user = user;
+				req.flash('success','注册成功');
+				res.redirect('/');
+			});
+		});
 	});
 
 	app.get('/login', function(req, res){
