b70e76f4a6346fdd593ffcd8d317a89ae68d1959
sumaolin <sumaolin619@yahoo.cn>
2013-12-06 15:55:55 +0800

    博客的路由配置

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,36 @@
  * GET home page.
  */
 module.exports = function(app){
-	app.get('/nswbmw',function(req, res){
-		res.send("Hello World");
-	})
 	app.get('/',function(req, res){
-		res.render('index',{title:"Express 3"});
+		res.render('index', {title:"主页"});
+	});
+
+	app.get('/reg', function(req, res){
+		res.render('reg', {title:'注册'});
+	});
+
+	app.post('/reg', function(req, res){
+
+	});
+
+	app.get('/login', function(req, res){
+		res.render('login', {title:'登录'});
+	})
+
+	app.post('/login', function(req, res){
+
+	});
+
+	app.get('/post', function(req, res){
+		res.render('post', {title:'发表'});
+	});
+
+	app.post('/post', function(req, res){
+
+	});
+
+	app.get('/logout', function(req, res){
+		res.render('logout', {title:'退出'});
 	});
 }
 // exports.index = function(req, res){
