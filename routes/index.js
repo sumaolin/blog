bae4dc4a034a0fa11f8c6a6d8228e1472f9aa648
sumaolin <sumaolin619@yahoo.cn>
2013-12-11 17:14:38 +0800

    按用户名，时间，标题获取文章的路由逻辑

--- routes/index.js
+++ routes/index.js
@@ -167,6 +167,48 @@ module.exports = function(app){
 		res.redirect('/upload');
 	});
 
+	app.get('/u/:name',function(req, res){  //用户文章列表
+		User.get(req.params.name, function(err,user){
+			if(!user){
+				req.flash('error','用户不存在');
+				return  res.redirect('/');
+			}
+
+			Post.getAll(user.name, function(err, posts){
+				if(err){
+					req.flash('error',err);
+					return res.redirect('/');
+				}
+
+				res.render('user',{
+					title: user.name,
+					posts: posts,
+					user: req.session.user,
+					success: req.flash('success').toString(),
+					error: req.flash('error').toString()
+				});
+			});
+		});
+	});
+
+
+	app.get('/u/:name/:day/:title', function(req,res){
+		Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post){
+			if(err){
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+
+			res.render('article',{
+				title: req.params.title,
+				post: post,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
 	function checkLogin(req, res, next){
 		if(!req.session.user){
 			req.flash('error', '登录');
