86ed8204b612369eadf2cc3119bfc4b4f07840a6
sumaolin <sumaolin619@yahoo.cn>
2013-12-18 15:17:57 +0800

    显示所有tags页面

--- models/post.js
+++ models/post.js
@@ -259,3 +259,27 @@ Post.getArchive = function(callback){
 		});
 	});
 };
+
+Post.getTags = function(callback){
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.distinct('tags', function(err, docs){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+
+				callback(null, docs);
+			})
+		});
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -353,6 +353,23 @@ module.exports = function(app){
 		});
 	});
 
+	app.get('/tags', function(req, res){
+		Post.getTags(function(err, posts){
+			if(err){
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+
+			res.render('tags', {
+				title: '标签',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
 	function checkLogin(req, res, next){
 		if(!req.session.user){
 			req.flash('error', '登录');
